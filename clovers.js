b1e5cb1fe9c149589cfb6d3cfa3cf75ab3f7e8ff
Boonch342 <123705578+Boonch342@users.noreply.github.com>
2024-05-21 12:10:34 -0500

    Create clovers.js

---
+++ clovers.js
@@ -0,0 +1,7 @@
+Game.Notify(`Icons have changed!`, `Let\'s spice it up a bit`, [28,12],10,1);
+		
+Game.Upgrades['Lucky day'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/clovers.png'];
+Game.Upgrades['Serendipity'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/clovers.png'];
+Game.Upgrades['Get lucky'].icon = [2,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/clovers.png'];
+Game.Upgrades['Residual luck'].icon = [3,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/clovers.png'];
+Game.Achievements['Four-leaf cookie'].icon = [4,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/clovers.png'];
