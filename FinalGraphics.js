3755195acc50e21b689a66346c4775202d967a0a
Boonch342 <123705578+Boonch342@users.noreply.github.com>
2024-10-28 15:04:10 -0500

    Update FinalGraphics.js

--- FinalGraphics.js
+++ FinalGraphics.js
@@ -73,5 +73,7 @@ Game.Achievements['Uncanny clicker'].icon = [13,36,'https://images-wixmp-ed30a86
 
 Game.Achievements['Make my day'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/makemyday.png'];
 Game.Achievements['Buy buy buy'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/buybuybuy.png'];
+
+//These two are from the Spiced Cookies mod.
 Game.Achievements['Parasitesmasher'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/rockwrinkler.png']
 Game.Achievements['A sleightly longer grind'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/RockDeer.png']
