5d3dcbc101c992406a3495a936ac19c65b0aac90
Boonch342 <123705578+Boonch342@users.noreply.github.com>
2024-12-06 10:52:55 -0600

    Update FinalGraphics.js

--- FinalGraphics.js
+++ FinalGraphics.js
@@ -1,3 +1,4 @@
+//Misc achievements
 Game.Achievements['Eldeer'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Eldeer/Eldeer.png'];
 Game.Upgrades['Basic wallpaper assortment'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Wallpapers/icons.png'];
 Game.Upgrades['Distinguished wallpaper assortment'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Wallpapers/icons.png'];
@@ -6,74 +7,90 @@ Game.Achievements['Jellicles'].icon = [26,7];
 Game.Achievements['Wrinkler poker'].icon = [0, 0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/poked/poked.png'];
 Game.Achievements['True Neverclick'].icon = [11,36,'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f7b51dea-3030-45c1-b349-ef3621aaba3d/dfcsef9-1444c288-63d1-47ef-9afb-28ce08262b39.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y3YjUxZGVhLTMwMzAtNDVjMS1iMzQ5LWVmMzYyMWFhYmEzZFwvZGZjc2VmOS0xNDQ0YzI4OC02M2QxLTQ3ZWYtOWFmYi0yOGNlMDgyNjJiMzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PQdSVu6vZzNc-YagKLHxk2Z51RfL6cI7m0Btktisvio'];
 Game.Achievements['Uncanny clicker'].icon = [13,36,'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f7b51dea-3030-45c1-b349-ef3621aaba3d/dfcsef9-1444c288-63d1-47ef-9afb-28ce08262b39.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y3YjUxZGVhLTMwMzAtNDVjMS1iMzQ5LWVmMzYyMWFhYmEzZFwvZGZjc2VmOS0xNDQ0YzI4OC02M2QxLTQ3ZWYtOWFmYi0yOGNlMDgyNjJiMzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PQdSVu6vZzNc-YagKLHxk2Z51RfL6cI7m0Btktisvio'];
-		Game.Achievements['Lord of Progress'].icon = [9, 13]; 
-		Game.Achievements['The full picture'].icon = [9, 14]; 
-		Game.Achievements['When there\'s nothing left to add'].icon = [9, 15]; 
-		Game.Achievements['Kaizen'].icon = [9, 16]; 
-		Game.Achievements['Beyond quality'].icon = [9, 17]; 
-		Game.Achievements['Oft we mar what\'s well'].icon = [9, 18];
-		
-		
-		Game.Achievements['Bicentennial and a half'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Tricentennial'].icon = [9,6];
-		Game.Achievements['Tricentennial and a half'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Quadricentennial'].icon = [1,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Quadricentennial and a half'].icon = [1,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Quincentennial'].icon = [1,4,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Quincentennial and a half'].icon = [1,5,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Sexcentennial'].icon = [29,25];
-		Game.Achievements['Sexcentennial and a half'].icon = [29,26];
-		Game.Achievements['Septcentennial'].icon = [1,7,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		
-		Game.Achievements['Lord of Constructs'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Grand design'].icon = [5,6];
-		Game.Achievements['Ecumenopolis'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
-		Game.Achievements['Myriad'].icon = [0,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
 
-		Game.Upgrades['Farmer grandmas'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Miner grandmas'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Worker grandmas'].icon = [2,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Banker grandmas'].icon = [3,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Priestess grandmas'].icon = [4,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
-		Game.Upgrades['Witch grandmas'].icon = [5,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Cosmic grandmas'].icon = [6,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Transmuted grandmas'].icon = [7,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
-		Game.Upgrades['Altered grandmas'].icon = [8,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
-		Game.Upgrades['Grandmas\' grandmas'].icon = [9,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Antigrandmas'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Rainbow grandmas'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Lucky grandmas'].icon = [2,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Metagrandmas'].icon = [3,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Binary grandmas'].icon = [4,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Alternate grandmas'].icon = [5,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Brainy grandmas'].icon = [6,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Clone grandmas'].icon = [10,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Upgrades['Banker grandmas'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/iconGrandmonocle.png'];
-		
-		Game.Achievements['Just wrong'].icon = [9,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Achievements['Elder'].icon = [6,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Achievements['Veteran'].icon = [3,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Achievements['Baby it\'s old outside'].icon = [7,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
-		Game.Achievements['In her likeness'].icon = [10,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+//These are from the Building Pillar icon update mod or whatever it's called
+//These are the total upgrade achievemnets
+Game.Achievements['Lord of Progress'].icon = [9, 13]; 
+Game.Achievements['The full picture'].icon = [9, 14]; 
+Game.Achievements['When there\'s nothing left to add'].icon = [9, 15]; 
+Game.Achievements['Kaizen'].icon = [9, 16]; 
+Game.Achievements['Beyond quality'].icon = [9, 17]; 
+Game.Achievements['Oft we mar what\'s well'].icon = [9, 18];
 
-		Game.Loader.Replace('bankGrandma.png','https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/bankGrandmonocle.png');
+//The ruler achievements
+Game.Achievements['Bicentennial and a half'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Tricentennial'].icon = [9,6];
+Game.Achievements['Tricentennial and a half'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Quadricentennial'].icon = [1,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Quadricentennial and a half'].icon = [1,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Quincentennial'].icon = [1,4,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Quincentennial and a half'].icon = [1,5,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Sexcentennial'].icon = [29,25];
+Game.Achievements['Sexcentennial and a half'].icon = [29,26];
+Game.Achievements['Septcentennial'].icon = [1,7,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
 
-		Game.Achievements['Resurrection'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		Game.Achievements['Reincarnation'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		
-		Game.Achievements['Wrinklesquisher'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		Game.Achievements['Moistburster'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		
-		Game.Achievements['Sleigh of hand'].icon = [0,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		Game.Achievements['Reindeer sleigher'].icon = [1,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		Game.Achievements['Elder slumber'].icon = [0,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
-		Game.Achievements['Elder calm'].icon = [1,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+//The pillar achievements
+Game.Achievements['Lord of Constructs'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Grand design'].icon = [5,6];
+Game.Achievements['Ecumenopolis'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
+Game.Achievements['Myriad'].icon = [0,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/others/PillarRuler.png'];
 
+//Frok the updated grandma icons mod
+//Grandma synergy icons
+Game.Upgrades['Farmer grandmas'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Miner grandmas'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Worker grandmas'].icon = [2,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Banker grandmas'].icon = [3,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Priestess grandmas'].icon = [4,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
+Game.Upgrades['Witch grandmas'].icon = [5,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Cosmic grandmas'].icon = [6,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Transmuted grandmas'].icon = [7,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
+Game.Upgrades['Altered grandmas'].icon = [8,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png']; 
+Game.Upgrades['Grandmas\' grandmas'].icon = [9,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Antigrandmas'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Rainbow grandmas'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Lucky grandmas'].icon = [2,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Metagrandmas'].icon = [3,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Binary grandmas'].icon = [4,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Alternate grandmas'].icon = [5,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Brainy grandmas'].icon = [6,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Clone grandmas'].icon = [10,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Upgrades['Banker grandmas'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/iconGrandmonocle.png'];
+
+//Misc grandma icons
+Game.Achievements['Just wrong'].icon = [9,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Achievements['Elder'].icon = [6,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Achievements['Veteran'].icon = [3,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Achievements['Baby it\'s old outside'].icon = [7,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+Game.Achievements['In her likeness'].icon = [10,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/coolGrannies.png'];
+
+//Grandma monocle for banker grandmas
+Game.Loader.Replace('bankGrandma.png','https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/Grandma/bankGrandmonocle.png');
+
+//The triple achievements mod
+//Acend # number times
+Game.Achievements['Resurrection'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+Game.Achievements['Reincarnation'].icon = [1,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+
+//Pop # wrinklers 
+Game.Achievements['Wrinklesquisher'].icon = [0,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+Game.Achievements['Moistburster'].icon = [1,1,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+
+//Pop # reindeer
+Game.Achievements['Sleigh of hand'].icon = [0,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+Game.Achievements['Reindeer sleigher'].icon = [1,2,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+
+//Pledge # times
+Game.Achievements['Elder slumber'].icon = [0,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+Game.Achievements['Elder calm'].icon = [1,3,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/tripleachievs/tripleCheevos.png'];
+
+//These are two icons I made because the original icons are bland
+//The "Make a day" and "spend a day" of cps in the stock market
 Game.Achievements['Make my day'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/makemyday.png'];
 Game.Achievements['Buy buy buy'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/buybuydollarsign.png'];
 
-//These two are from the Spiced Cookies mod.
+//These two are the ones the Spiced Cookies mod adds
 Game.Achievements['Parasitesmasher'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/rockwrinkler.png']
 Game.Achievements['A sleightly longer grind'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/main/RockDeer.png']
 
-Game.Achievements['Quite fine'].icon = [0,0,'https://raw.githubusercontent.com/Boonch342/MinorGraphicsCC/refs/heads/main/Opti-Face.png'];
+//There used to be the one for the naming yourself Opti from the modded Dark Cheevos, but I just put the image directly into the modded mod
